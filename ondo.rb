634af46e76a2cc6190b596156039ad9c2d5f764b
Nick Chaffee <nchafee@gmail.com>
2021-12-06 09:36:41 -0500

    debug tz on heroku

--- ondo.rb
+++ ondo.rb
@@ -5,16 +5,18 @@ require 'sinatra/json'
 require 'tzinfo'
 
 get '/' do
-    timezone = TZInfo::Timezone.get('US/Eastern')
+    offset = TZInfo::Timezone.get('US/Eastern').utc_offset
+    now = Time.now.getlocal(offset)
 
     uri = URI('https://api.weather.gov/gridpoints/BTV/89,55')
     response = Net::HTTP.get(uri)
     data = JSON.parse(response)
     ondo = data["properties"]["temperature"]["values"].map do |values|
-        parsedTime = values["validTime"].gsub('+00:00','Z')[0..19].split(/\D/)
-        { time: Time.new(*parsedTime, timezone.utc_offset), temp: values["value"]}
-    end.min_by{|pair| (Time.now - pair[:time]).abs}[:temp]
-    
+        parsed_time = values["validTime"].gsub('+00:00','Z')[0..19].split(/\D/)
+        new_time = Time.new(*parsed_time, offset)
+        { time: new_time, temp: values["value"]}
+    end.min_by{|pair| (now - pair[:time]).abs}[:temp]
+
     json :ondo => ondo
 end
 
