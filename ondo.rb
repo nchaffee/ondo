b83efc9d1cce73569d73539eb6297d8b48bfe68a
Nick Chaffee <nchafee@gmail.com>
2021-12-06 09:34:19 -0500

    fix offset

--- ondo.rb
+++ ondo.rb
@@ -6,14 +6,14 @@ require 'tzinfo'
 
 get '/' do
     timezone = TZInfo::Timezone.get('US/Eastern')
-    now = Time.now.getlocal(timezone.utc_offset)
+
     uri = URI('https://api.weather.gov/gridpoints/BTV/89,55')
     response = Net::HTTP.get(uri)
     data = JSON.parse(response)
     ondo = data["properties"]["temperature"]["values"].map do |values|
         parsedTime = values["validTime"].gsub('+00:00','Z')[0..19].split(/\D/)
-        { time: Time.new(*parsedTime), temp: values["value"]}
-    end.min_by{|pair| (now - pair[:time]).abs}[:temp]
+        { time: Time.new(*parsedTime, timezone.utc_offset), temp: values["value"]}
+    end.min_by{|pair| (Time.now - pair[:time]).abs}[:temp]
     
     json :ondo => ondo
 end
