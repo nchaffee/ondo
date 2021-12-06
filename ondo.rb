203253efa13b0f0280f9e650303bfcb5bc476aa9
Nick Chaffee <nchafee@gmail.com>
2021-12-06 09:30:09 -0500

    always use Eastern timezone

--- ondo.rb
+++ ondo.rb
@@ -2,19 +2,18 @@ require 'json'
 require 'net/http'
 require 'sinatra'
 require 'sinatra/json'
+require 'tzinfo'
 
 get '/' do
+    timezone = TZInfo::Timezone.get('US/Eastern')
+    now = Time.now.getlocal(timezone.utc_offset)
     uri = URI('https://api.weather.gov/gridpoints/BTV/89,55')
-    res = Net::HTTP.get(uri)
-    data = JSON.parse(res)
-    props = data["properties"]
-    temp = props["temperature"]
-    temps = temp["values"]
-    times = temps.map do |values|
+    response = Net::HTTP.get(uri)
+    data = JSON.parse(response)
+    ondo = data["properties"]["temperature"]["values"].map do |values|
         parsedTime = values["validTime"].gsub('+00:00','Z')[0..19].split(/\D/)
         { time: Time.new(*parsedTime), temp: values["value"]}
-    end
-    ondo = times.min_by{|pair| (Time.now - pair[:time]).abs}[:temp]
+    end.min_by{|pair| (now - pair[:time]).abs}[:temp]
     
     json :ondo => ondo
 end
